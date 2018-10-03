65c060ba160859fde8740b53ea0b15e7938e7567
Berk Erkul <berker@microsoft.com>
2018-10-02 17:15:48 -0700

    add projectile prototype

---
+++ Dorothy/Assets/Scripts/Follow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject target;
+    public Vector3 offset;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = target.transform.position + offset;
+    }
+}
---
+++ Dorothy/Assets/Scripts/Thruster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thruster : MonoBehaviour
+{
+    public GameObject thrustTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Quaternion yawStickRot = thrusterStick.transform.rotation;            
+            float magnitude = 50.0f;
+            Vector3 relativePosition = thrustTarget.transform.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePosition);
+            Vector3 direction = rotation * Vector3.forward;
+
+            gameObject.GetComponent<Rigidbody>().AddForce(direction * magnitude, ForceMode.Impulse);
+        }
+    }
+}
